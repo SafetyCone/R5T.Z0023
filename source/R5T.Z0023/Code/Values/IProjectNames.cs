5f22aa1843070ce90779bf4bd74dec12705e5b3f
davidcoats <david@getsafetycone.com>
2024-12-04 23:19:11 -0800

    Interim commit.

--- source/R5T.Z0023/Code/Values/IProjectNames.cs
+++ source/R5T.Z0023/Code/Values/IProjectNames.cs
@@ -8,6 +8,9 @@ namespace R5T.Z0023
 	[ValuesMarker]
 	public partial interface IProjectNames : IValuesMarker
 	{
+		/// <summary>
+		/// <para><value>Example</value></para>
+		/// </summary>
 		public string Example => "Example";
 	}
 }
--- source/R5T.Z0023/Project Plan.md
+++ source/R5T.Z0023/Project Plan.md
@@ -1,6 +1,7 @@
 # R5T.Z0023
 Example code unit values (project names, repository descriptions, etc.).
 
+
 ## Prior Work
 
 * R5T.Z0004 - IExamples.LibraryName, etc.
